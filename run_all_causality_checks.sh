4859f4b45492e0700de19a5125a51cd2f7c6ec60
Christopher Plumberg <6557786+astrophysicist87@users.noreply.github.com>
2021-06-28 13:59:43 -0500

    Forgot that new OSU_hydro output needs to run with 3+1D version!

--- run_all_causality_checks.sh
+++ run_all_causality_checks.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-sbatch --export=ALL,file_to_check=`realpath ../OSU_hydro/test_all/viscous_14_moments_evo.dat` run_causality_check.sbatch
-
-for file in ../MUSIC/v8/PbPb_*Kompost/results/evolution_full.dat
+for file in ../OSU_hydro/test_all/viscous_14_moments_evo.dat ../MUSIC/v8/PbPb_*Kompost/results/evolution_full.dat
 do
 	sbatch --export=ALL,file_to_check=`realpath $file` run_causality_check_3_plus_1D.sbatch
 done
