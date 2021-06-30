eb082d95ecbe310f495c6fd71bdb4d26e88f7b22
Christopher Plumberg <6557786+astrophysicist87@users.noreply.github.com>
2021-06-30 12:39:15 -0500

    Process small systems as well

--- run_all_causality_checks.sh
+++ run_all_causality_checks.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-for file in ../OSU_hydro/test_all/viscous_14_moments_evo.dat ../MUSIC/v8/PbPb_*Kompost/results/evolution_full.dat
+#for file in ../OSU_hydro/test_all/viscous_14_moments_evo.dat \
+#			../MUSIC/v8/PbPb_*Kompost/results/evolution_full.dat
+#do
+#	sbatch --export=ALL,file_to_check=`realpath $file` run_causality_check_3_plus_1D.sbatch
+#done
+
+for file in ../OSU_hydro/RESULTS_pPb_OPTIMAL/viscous_14_moments_evo.dat \
+			../MUSIC/v8/pPb_noKompost/results/evolution_full.dat \
+			../MUSIC/v8/OO_noKompost/results/evolution_full.dat
 do
 	sbatch --export=ALL,file_to_check=`realpath $file` run_causality_check_3_plus_1D.sbatch
 done
