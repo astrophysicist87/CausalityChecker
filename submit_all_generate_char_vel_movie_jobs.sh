5f9bd4c271613ba065390aa52660b27881efb171
Christopher Plumberg <6557786+astrophysicist87@users.noreply.github.com>
2021-06-30 13:07:47 -0500

    Update submit_all_generate_char_vel_movie_jobs.sh
    Change this to run only on VISHNU event

--- submit_all_generate_char_vel_movie_jobs.sh
+++ submit_all_generate_char_vel_movie_jobs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Pb Pb - Vishnu
-#for file in ../OSU_hydro/test_all/causality_check_w_characteristic_velocities.dat
-#do
-#	sbatch --export=ALL,causality_check_file=`readlink -e $file`,gridScale=16.7778,gridSize=$[235**2],eDecoupling=0.26511743083794326,dxy=0.1434 generate_char_vel_movie.sbatch
-#done
+for file in ../OSU_hydro/test_all/causality_check_w_characteristic_velocities.dat
+do
+	sbatch --export=ALL,causality_check_file=`readlink -e $file`,gridScale=16.7778,gridSize=$[235**2],eDecoupling=0.26511743083794326,dxy=0.1434 generate_char_vel_movie.sbatch
+done
 
 
 # Pb Pb - MUSIC
@@ -15,22 +15,22 @@
 
 
 # p Pb - Vishnu
-for file in ../OSU_hydro/test_all/causality_check_w_characteristic_velocities.dat
-do
-	sbatch --export=ALL,causality_check_file=`readlink -e $file`,gridScale=16.7778,gridSize=$[235**2],eDecoupling=0.26511743083794326,dxy=0.1434 generate_char_vel_movie.sbatch
-done
+#for file in ../OSU_hydro/test_all/causality_check_w_characteristic_velocities.dat
+#do
+#	sbatch --export=ALL,causality_check_file=`readlink -e $file`,gridScale=16.7778,gridSize=$[235**2],eDecoupling=0.26511743083794326,dxy=0.1434 generate_char_vel_movie.sbatch
+#done
 
 
 # p Pb - MUSIC
-for file in ../MUSIC/v8/PbPb_*Kompost/results/causality_check_w_characteristic_velocities.dat
-do
-	sbatch --export=ALL,causality_check_file=`readlink -e $file`,gridScale=20.48,gridSize=$[512**2],eDecoupling=0.18,dxy=0.08 generate_char_vel_movie.sbatch
-done
+#for file in ../MUSIC/v8/PbPb_*Kompost/results/causality_check_w_characteristic_velocities.dat
+#do
+#	sbatch --export=ALL,causality_check_file=`readlink -e $file`,gridScale=20.48,gridSize=$[512**2],eDecoupling=0.18,dxy=0.08 generate_char_vel_movie.sbatch
+#done
 
 
 # O O - MUSIC
-for file in ../MUSIC/v8/PbPb_*Kompost/results/causality_check_w_characteristic_velocities.dat
-do
-	sbatch --export=ALL,causality_check_file=`readlink -e $file`,gridScale=20.48,gridSize=$[512**2],eDecoupling=0.18,dxy=0.08 generate_char_vel_movie.sbatch
-done
+#for file in ../MUSIC/v8/PbPb_*Kompost/results/causality_check_w_characteristic_velocities.dat
+#do
+#	sbatch --export=ALL,causality_check_file=`readlink -e $file`,gridScale=20.48,gridSize=$[512**2],eDecoupling=0.18,dxy=0.08 generate_char_vel_movie.sbatch
+#done
 
