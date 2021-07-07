7a97f8f098c92f7f32a454573aa0f9fc492d4628
Christopher Plumberg <6557786+astrophysicist87@users.noreply.github.com>
2021-07-07 16:31:22 -0500

    Update plot_charvel_vs_tau_e.py

--- plot_charvel_vs_tau_e.py
+++ plot_charvel_vs_tau_e.py
@@ -22,11 +22,11 @@ def load_file(filename):
     data = np.c_[ data[:,0], 0.197327*data[:,1],
                   np.amin(data[:,2:], axis=1), np.amax(data[:,2:], axis=1) ]
     # max violation histogram
-    hist0, bins0 = np.histogram(data[:,1], bins=nebins)
-    hist, bins = np.histogram(data[:,1], bins=nebins, weights=np.heaviside(np.sqrt(data[:,3])-1.0,0.0))
+    hist0, bins0 = np.histogram(data[:,1], bins=ebins)
+    hist, bins = np.histogram(data[:,1], bins=ebins, weights=np.heaviside(np.sqrt(data[:,3])-1.0,0.0))
     #print(bins.size)
     #print(hist.size)
-    return np.c_[ data[0,0]*np.ones(hist.size), 0.5*(bins[:-1]+bins[1:]), (hist+1e-100)/(hist0+1e-10) ]
+    return np.c_[ data[0,0]*np.ones(hist.size), 0.5*(bins[:-1]+bins[1:]), hist/(hist0+1e-100) ]
     
 #====================================================================================
 if __name__ == "__main__":
