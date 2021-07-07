444cf57f31a8c6f177826741f1593410c2f2b1a5
Christopher Plumberg <6557786+astrophysicist87@users.noreply.github.com>
2021-07-07 18:20:09 -0500

    Update plot_charvel_vs_tau_e.py

--- plot_charvel_vs_tau_e.py
+++ plot_charvel_vs_tau_e.py
@@ -28,30 +28,27 @@ def load_file(filename):
     hist0, bins0 = np.histogram(data[:,1], bins=ebins, weights=np.ones(data[:,1].size))
     w = np.sqrt(data[:,3])-1.0
     hist, bins = np.histogram(data[:,1], bins=ebins, weights=w*np.heaviside(w,0.0))
-    tmp = np.c_[ data[:,0], data[:,1], data[:,3], np.heaviside(np.sqrt(data[:,3])-1.0,0.0) ]
-    print(tmp[np.where(tmp[:,1]>400.0)])
+    #tmp = np.c_[ data[:,0], data[:,1], data[:,3], np.heaviside(np.sqrt(data[:,3])-1.0,0.0) ]
+    #print(tmp[np.where(tmp[:,1]>400.0)])
     #print(bins.size)
     #print(hist.size)
-    return np.c_[ data[0,0]*np.ones(hist.size), 0.5*(bins[:-1]+bins[1:]), hist/(hist0+1e-100), hist, hist0 ]
+    return np.c_[ data[0,0]*np.ones(hist.size), 0.5*(bins[:-1]+bins[1:]), hist/(hist0+1e-100) ]
     
 #====================================================================================
 if __name__ == "__main__":
-    #for filename in sys.argv[1:]:
-    #    print(load_file(filename).shape)
     dataToPlot = np.array([load_file(filename) for filename in sys.argv[1:11]])
     print(dataToPlot.shape)
     print(dataToPlot.size)
-    #dataToPlot = dataToPlot.reshape( len(dataToPlot)/(3*ebins.size), ebins.size, 3 )
     fig, ax = plt.subplots( nrows=1, ncols=1 )
     psm = ax.pcolormesh(dataToPlot[:,:,0], dataToPlot[:,:,1], dataToPlot[:,:,2], shading='nearest')
     fig.colorbar(psm, ax=ax)
     
-    print(dataToPlot.shape)
-    print(len(dataToPlot))
+    #print(dataToPlot.shape)
+    #print(len(dataToPlot))
     newLen=dataToPlot.size
-    print(newLen)
-    print(newLen/5)
-    toSave = dataToPlot.reshape([int(newLen/5),5])
+    #print(newLen)
+    #print(newLen/3)
+    toSave = dataToPlot.reshape([int(newLen/3),3])
     np.savetxt('./charvel_density.dat', toSave, fmt='%12.8f')
     
     #plt.show()
