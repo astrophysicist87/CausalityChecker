8ab16832bf86bd8c701e5ad90930f92a1fafe3c5
Christopher Plumberg <6557786+astrophysicist87@users.noreply.github.com>
2021-07-07 16:23:46 -0500

    Update plot_charvel_vs_tau_e.py

--- plot_charvel_vs_tau_e.py
+++ plot_charvel_vs_tau_e.py
@@ -26,7 +26,7 @@ def load_file(filename):
     hist, bins = np.histogram(data[:,1], bins=nebins, weights=np.heaviside(np.sqrt(data[:,3])-1.0,0.0))
     #print(bins.size)
     #print(hist.size)
-    return np.c_[ data[0,0]*np.ones(hist.size), 0.5*(bins[:-1]+bins[1:]), hist/(hist0+1e-100) ]
+    return np.c_[ data[0,0]*np.ones(hist.size), 0.5*(bins[:-1]+bins[1:]), (hist+1e-100)/(hist0+1e-10) ]
     
 #====================================================================================
 if __name__ == "__main__":
