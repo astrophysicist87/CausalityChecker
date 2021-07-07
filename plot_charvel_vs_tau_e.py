ed9a61365a55116440b34babbc211bee8c226b93
Christopher Plumberg <6557786+astrophysicist87@users.noreply.github.com>
2021-07-07 11:47:48 -0500

    Create plot_charvel_vs_tau_e.py

---
+++ plot_charvel_vs_tau_e.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+from matplotlib import cm
+import os, sys
+
+nebins = 800  # fix this somehow
+ebins = np.arange(0,nebins)
+
+#====================================================================================
+def get_ncols(filename):
+    with open(filename) as f:
+        n_cols = len(f.readline().split())
+    f.close()
+    return n_cols
+
+#====================================================================================
+def load_file(filename):
+    data = np.loadtxt( filename, usecols=tuple([2,6]+list(range(9, n_cols))) )
+    data = np.c_[ data[:,0], 0.197327*data[:,1],
+                  np.amin(data[:,2:], axis=1), np.amax(data[:,2:], axis=1) ]
+    # max violation histogram
+    hist, bins = np.histogram(data[:,1], bins=nebins, weights=data[:,3])
+    yield np.c_[ data[0,0]*np.ones(hist.size), bins, hist ]
+    
+#====================================================================================
+if __name__ == "__main__":
+    dataToPlot = np.array([load_file(filename) for filename in sys.argv[1:]])
+    #print(dataToPlot.shape)
+    #dataToPlot = dataToPlot.reshape( len(dataToPlot)/(3*ebins.size), ebins.size, 3 )
+    fig, ax = plt.subplots( nrows=1, ncols=1 )
+    ax.pcolormesh(data[:,:,0], data[:,:,1], data[:,:,2])
+    
+    #plt.show()
+    outfilename = './charvel_density_plot.png'
+    print('Saving to', outfilename)
+    fig.savefig(outfilename, bbox_inches='tight')
+    plt.close(fig)
+
+    
+        
