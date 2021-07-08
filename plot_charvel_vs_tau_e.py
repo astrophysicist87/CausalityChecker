c78e344f9dabd665f0061c2b06ba1da7932eb5ab
Christopher Plumberg <6557786+astrophysicist87@users.noreply.github.com>
2021-07-07 19:49:33 -0500

    Update plot_charvel_vs_tau_e.py

--- plot_charvel_vs_tau_e.py
+++ plot_charvel_vs_tau_e.py
@@ -38,7 +38,7 @@ def load_file(filename):
     
 #====================================================================================
 if __name__ == "__main__":
-    dataToPlot = np.array([load_file(filename) for filename in sys.argv[1:81]])
+    dataToPlot = np.array([load_file(filename) for filename in sys.argv[1:31]])
     #print(dataToPlot.shape)
     #print(dataToPlot.size)
     fig, ax = plt.subplots( nrows=1, ncols=1 )
