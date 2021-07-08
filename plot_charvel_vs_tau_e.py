c36395ad3f7f59cb49d0b0cf18db2fbd3b68ea95
Christopher Plumberg <6557786+astrophysicist87@users.noreply.github.com>
2021-07-07 20:50:24 -0500

    Update plot_charvel_vs_tau_e.py

--- plot_charvel_vs_tau_e.py
+++ plot_charvel_vs_tau_e.py
@@ -42,7 +42,11 @@ if __name__ == "__main__":
     #print(dataToPlot.shape)
     #print(dataToPlot.size)
     fig, ax = plt.subplots( nrows=1, ncols=1 )
-    psm = ax.pcolormesh(dataToPlot[:,:,0], dataToPlot[:,:,1], dataToPlot[:,:,2], shading='nearest')
+    #psm = ax.pcolormesh(dataToPlot[:,:,0], dataToPlot[:,:,1], dataToPlot[:,:,2], shading='nearest')
+    x = dataToPlot[:,:,0]
+    y = dataToPlot[:,:,1]
+    extent = np.min(x), np.max(x), np.min(y), np.max(y)
+    psm = plt.imshow(dataToPlot[:,:,2], interpolation='bicubic', extent=extent)
     fig.colorbar(psm, ax=ax)
     
     #print(dataToPlot.shape)
