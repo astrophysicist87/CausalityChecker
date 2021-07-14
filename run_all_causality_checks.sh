1ad64096caa9d97565113dc50364e23d56c78edf
Christopher Plumberg <6557786+astrophysicist87@users.noreply.github.com>
2021-07-14 11:41:43 -0500

    Fix typo

--- run_all_causality_checks.sh
+++ run_all_causality_checks.sh
@@ -9,7 +9,7 @@
 #for file in ../OSU_hydro/RESULTS_pPb_OPTIMAL/viscous_14_moments_evo.dat \
 #			../MUSIC/v8/pPb_noKompost/results/evolution_full.dat \
 #			../MUSIC/v8/OO_noKompost/results/evolution_full.dat
-for file in ../OSU_hydro/RESULTS_pPb_OPTIMAL_nc6/viscous_14_moments_evo.dat \
+for file in ../OSU_hydro/RESULTS_pPb_OPTIMAL_nc6/viscous_14_moments_evo.dat
 do
 	sbatch --export=ALL,file_to_check=`realpath $file` run_causality_check_3_plus_1D.sbatch
 done
