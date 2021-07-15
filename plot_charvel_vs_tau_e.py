f04ed8c6b226ce9aa8601fc9ce04ee5cb310a601
Christopher Plumberg <6557786+astrophysicist87@users.noreply.github.com>
2021-07-14 20:04:27 -0500

    Update plot_charvel_vs_tau_e.py

--- plot_charvel_vs_tau_e.py
+++ plot_charvel_vs_tau_e.py
@@ -12,7 +12,7 @@ maxDeltatau = 0.05 # maximum duration to plot
 #nebins = 200  # fix this somehow
 #nTbins = 50
 ebins = np.arange(0,2.0,0.01)
-Tbins = np.arange(125.0,225.0,2.0)
+Tbins = np.arange(125.0,225.0,0.5)
 nebins = len(ebins)
 nTbins = len(Tbins)
 
