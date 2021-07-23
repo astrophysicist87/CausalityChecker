8042d561599f9bf06f002d2d77b14fe8abde435f
Plumberg <plumberg@golubh3.campuscluster.illinois.edu>
2021-07-23 10:47:40 -0500

    Add script to generate all characteristic velocity plots.

---
+++ make_all_charvel_plots.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd /projects/jnorhos/plumberg/MUSIC/v8
+for direc in *
+do
+	echo 'Processing' $direc
+	python3 /projects/jnorhos/plumberg/CausalityChecker/plot_charvel_vs_tau_e.py 1 ${direc}/results/char_vel_frames/*
+	cp ${direc}/results/charvel_density_plot.png \
+	   ${direc}/results/charvel_density_plot_${direc}.png 
+done
+cd /projects/jnorhos/plumberg/CausalityChecker
+
+
+for direc in ../OSU_hydro/RESULTS_pPb_OPTIMAL_nc6 ../OSU_hydro/test_all
+do
+	python3 plot_charvel_vs_tau_e.py 0 ${direc}/char_vel_frames/*
+done
+
+cp ../OSU_hydro/test_all/charvel_density_plot.png \
+   ../OSU_hydro/test_all/charvel_density_plot_PbPb_VISHNU.png
+
+cp ../OSU_hydro/RESULTS_pPb_OPTIMAL_nc6/charvel_density_plot.png \
+   ../OSU_hydro/RESULTS_pPb_OPTIMAL_nc6/charvel_density_plot_pPb_VISHNU.png
