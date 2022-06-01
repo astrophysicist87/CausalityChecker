112213f2e7c24d8cb531b5632ddf6cd8faa30d5a
Christopher Plumberg <astrophysicist87@gmail.com>
2022-06-01 13:32:46 -0500

    Increase fontsizes

--- plot_charvel_vs_tau_e.py
+++ plot_charvel_vs_tau_e.py
@@ -38,73 +38,45 @@ def load_file(filename, i):
     if i==0:
         tau0 = tau
     print('Processing tau =', tau, flush=True)
-    #print(data.shape)
-    #print(data[:,2:].shape)
-    #print(np.amax(data[:,2:], axis=1).shape)
+
     data = np.c_[ data[:,0], 197.327*data[:,1], 0.197327*data[:,2],
                   np.amin(data[:,5:], axis=1), np.amax(data[:,5:], axis=1),
                   data[:,3], data[:,4] ]
     data = data[np.where( (data[:,5]==1) & (data[:,6]==1) )] # where causality analysis succeeded
     data[:,4] = np.array(list(map(lambda x : np.max([x,0.0]), data[:,4])))
+
     # max violation histogram
     w = np.sqrt(data[:,4])-1.0
     hist0, bins0 = np.histogram(data[:,1], bins=Tbins, weights=np.ones(data[:,1].size)*np.heaviside(w,0.0))
     hist, bins = np.histogram(data[:,1], bins=Tbins, weights=w*np.heaviside(w,0.0))
-    #print(bins.size)
-    #print(hist.size)
-    #print(np.c_[ data[0,0]*np.ones(hist.size), 0.5*(bins[:-1]+bins[1:]), hist/(hist0+1e-100), hist, hist0 ].shape)
     return np.c_[ data[0,0]*np.ones(hist.size), 0.5*(bins[:-1]+bins[1:]), hist/(hist0+1e-100), hist, hist0 ]
     
 #====================================================================================
 if __name__ == "__main__":
-    #dataToPlot = np.array([load_file(filename) for filename in sys.argv[1:81]])
     dataToPlot = np.array([load_file(filename,i) for i, filename in enumerate(sys.argv[2:])])
-    #print(dataToPlot.shape)
-    #print(dataToPlot.size)
-    #print(len(dataToPlot))
-    #print(np.array(list(map(len,dataToPlot))))
-    #print(np.where(np.array(list(map(len,dataToPlot)))>1))
-    #print(np.array(np.where(np.array(list(map(len,dataToPlot)))>1)[0]))
-    #print(np.where(np.asarray(list(map(len,dataToPlot)))>1).flatten())
-    #dataToPlot = dataToPlot[np.where(np.array(list(map(len,dataToPlot)))>1)[0]]
     dims = dataToPlot.shape
     dataToPlot = dataToPlot.reshape((dims[0]*dims[1],dims[2]))
     dataToPlot = dataToPlot[np.where(dataToPlot[:,0]>0)]
     dims2 = dataToPlot.shape
-    #print("dims =",dims)
-    #print("dims2 =",dims2)
+
     dataToPlot = dataToPlot.reshape((dims2[0]//dims[1],dims[1],dims[2]))
-    #(lambda x : x[np.where(np.array(list(map(len,x)))!=0)].astype(float))(np.array([f(x) for x in range(10)],dtype=object))
-    #print(dataToPlot.shape)
-    #print(dataToPlot.size)
-    #print(dataToPlot.shape)
-    #print(dataToPlot.size)
-    #print(len(dataToPlot))
-    #print("Also check:")
-    #for dataThing in dataToPlot:
-    #    print(dataThing.shape)
+
     fig, ax = plt.subplots( nrows=1, ncols=1 )
     psm = ax.pcolormesh(dataToPlot[:,:,0], dataToPlot[:,:,1], 1.0+dataToPlot[:,:,2], \
                         shading='gouraud', vmin = 1.0, vmax = 1.20, cmap=plt.get_cmap('magma'))
-    #x = dataToPlot[:,:,0]
-    #y = dataToPlot[:,:,1]
-    #extent = np.min(x), np.max(x), np.min(y), np.max(y)
-    #m = dataToPlot[:,:,2]
-    #psm = plt.imshow(m.T, interpolation='bilinear', extent=extent)
+
     cbar = fig.colorbar(psm, ax=ax)
     cbar.set_label(r'$v_{\mathrm{char}}/c$', size=16)
+    cbar.ax.tick_params(labelsize=12)
 
     xpts = np.linspace(np.min(dataToPlot[:,:,0]), np.max(dataToPlot[:,:,0]), 3)
     ax.plot(xpts, 0.0*xpts+TFOs[VISHNUorMUSICmode], color='white', ls='--')
     
-    #print(dataToPlot.shape)
-    #print(len(dataToPlot))
     newLen=dataToPlot.size
-    #print(newLen)
-    #print(newLen/3)
     toSave = dataToPlot.reshape([int(newLen/5),5])
     np.savetxt(dirname + '/../charvel_density_check.dat', toSave, fmt='%12.8f')
-    
+
+    ax.tick_params(axis='both', which='major', labelsize=12)
     ax.set_xlabel(r'$\tau$ (fm/$c$)', fontsize=16)
     ax.set_ylabel(r'$T$ (MeV)', fontsize=16)
     
