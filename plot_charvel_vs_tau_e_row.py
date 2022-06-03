9647cd0a5b0e9f2c5f6ac1e78e0ad7d83d202df1
Christopher Plumberg <astrophysicist87@gmail.com>
2022-06-03 13:50:11 -0500

    It doesn't

--- plot_charvel_vs_tau_e_row.py
+++ plot_charvel_vs_tau_e_row.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
         exit()
         
     nFiles = (len(sys.argv)-1)//2
-    fig, axs = plt.subplots( nrows=1, ncols=nFiles, figsize=(17.5,5), constrained_layout=True )
+    fig, axs = plt.subplots( nrows=1, ncols=nFiles, figsize=(17.5,5) )
     plt.subplots_adjust( hspace=-0.01, wspace=0.025 )
     
     # loop over all histograms to plot together
