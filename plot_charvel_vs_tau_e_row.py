058372309342d5c9f6f912592c2a3e4cbced6fa7
Christopher Plumberg <astrophysicist87@gmail.com>
2022-06-03 13:33:02 -0500

    Try again

--- plot_charvel_vs_tau_e_row.py
+++ plot_charvel_vs_tau_e_row.py
@@ -20,7 +20,8 @@ if __name__ == "__main__":
         exit()
         
     nFiles = (len(sys.argv)-1)//2
-    fig, axs = plt.subplots( nrows=1, ncols=nFiles )
+    fig, axs = plt.subplots( nrows=1, ncols=nFiles, figsize=(5,10) )
+    plt.subplots_adjust( hspace=-0.01, wspace=-0.01 )
     
     # loop over all histograms to plot together
     for i in range(nFiles):
@@ -34,19 +35,19 @@ if __name__ == "__main__":
         psm = axs[i].pcolormesh(dataToPlot[:,:,0], dataToPlot[:,:,1], 1.0+dataToPlot[:,:,2], \
                             shading='gouraud', vmin = 1.0, vmax = 1.20, cmap=plt.get_cmap('magma'))
 
-        #cbar = fig.colorbar(psm, ax=axs[i])
-        #cbar.set_label(r'$v_{\mathrm{char}}/c$', size=16)
-        #cbar.ax.tick_params(labelsize=14)
-
         xpts = np.linspace(np.min(dataToPlot[:,:,0]), np.max(dataToPlot[:,:,0]), 3)
         axs[i].plot(xpts, 0.0*xpts+TFO, color='white', ls='--')
 
         axs[i].tick_params(axis='both', which='major', labelsize=14)
         axs[i].set_xticks(np.arange(tau0, tau, 0.1))
         axs[i].set_xlabel(r'$\tau$ (fm/$c$)', fontsize=16)
-        axs[i].set_ylabel(r'$T$ (MeV)', fontsize=16)
     
-    #plt.show()
+    axs[0].set_ylabel(r'$T$ (MeV)', fontsize=16)
+
+    cbar = fig.colorbar(psm, ax=axs[-1])
+    cbar.set_label(r'$v_{\mathrm{char}}/c$', size=16)
+    cbar.ax.tick_params(labelsize=14)
+
     outfilename = "charvel_density_combined.png"
     print('Saving to', outfilename)
     fig.savefig(outfilename, bbox_inches='tight')
