2a4c9e68aedacb1bd4ae8e711a51bfbe9decaa5a
Christopher Plumberg <astrophysicist87@gmail.com>
2023-06-07 17:00:21 -0700

    Wrapper script

--- generate_light_cone_plots.sh
+++ generate_light_cone_plots.sh
@@ -1,34 +1,37 @@
 #! /bin/bash
 #-----------
 
+echo 'Starting at ' `date`
 directoryToProcess=$1
 
-echo 'Resetting frame directory'
+echo 'Resetting frame directory at ' `date`
 rm -rf $directoryToProcess/light_cone_frames && mkdir -p $directoryToProcess/light_cone_frames
 
 
 # for MUSIC
-echo 'Compute local speeds'
+echo 'Compute local speeds at ' `date`
 grep 'FLOWPROFILE: ' $directoryToProcess/mode_2.log \
                      | awk '{print $2, $3, $4, sqrt($9**2+$10**2+$11**2)}' \
                      > $directoryToProcess/local_speed.dat
 
 # combine causality and local speed data
-echo 'Consolidate light cone data'
+echo 'Consolidate light cone data at ' `date`
 ./get_light_cone_data_src/get_light_cone_data \
   $directoryToProcess/causality_check_w_characteristic_velocities.dat \
   $directoryToProcess/local_speed.dat \
   > $directoryToProcess/light_cone_data.dat
 
 # split into frames
-echo 'Split into frames'
+echo 'Split into frames at ' `date`
 split --lines=$2 -d --suffix-length=4 \
         $directoryToProcess/light_cone_data.dat \
         $directoryToProcess/light_cone_frames/light_cone_frames
 
 # generate a movie from these frames
-echo 'Generate movie'
+echo 'Generate movie at ' `date`
 python3 generate_light_cone_movie.py \
           $directoryToProcess/light_cone_frames/light_cone_frames*
+
+echo 'Finished at ' `date`
 
 # for VISHNU
---
+++ run_light_cone_scripts.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#----------
+
+bash generate_light_cone_plots.sh ../MUSIC/v8_bugfix_def_chi0_10/PbPb_noKompost/results/ $[512**2]
